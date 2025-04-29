e935760be3f147d742c6c8582b313de3693db2af
diego-siqueira <diego_sp_br@hotmail.com>
2025-04-29 17:48:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.post('/interactions', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Sigma Express bot listening on ${PORT}`));
